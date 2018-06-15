7b6f008be6711a172d287d33dc3bf6316ad58805
magpia13 <mag.pia13@gmail.com>
2018-06-15 08:45:14 +0200

    loadbtn

--- app/src/main/java/com/example/user/puzzle/PuzzleActivity.java
+++ app/src/main/java/com/example/user/puzzle/PuzzleActivity.java
@@ -1,13 +1,19 @@
 package com.example.user.puzzle;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -23,21 +29,38 @@ public class PuzzleActivity extends AppCompatActivity {
 
     ArrayList<PuzzlePiece> parts;
     int click = 0;
-
+    ImageView img,sampleImage;
+    private Sensor mySensor;
+    private SensorManager SM;
+    //image
+    BitmapDrawable drawable;
+    Bitmap srcBmp;
+    ImageView Puzzle;
+    Bitmap[] bitmapList = new Bitmap[9];
+    Bitmap[] imgs = new Bitmap[9];
+    Bitmap[] imgs_zapas = new Bitmap[9];
+    Bitmap[] zmiana = new Bitmap[2];
+    ImageView[] ImageList =new ImageView[9];
+    int[] tab = new int[9];
+    int poziom=0, pion=0, width, height;
+    boolean res=true, hide=true;
+    public static  Uri imageUri;
+    private static final int PICK_IMAGE=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
 
         final RelativeLayout layout = findViewById(R.id.layout);
-        final ImageView imageView = findViewById(R.id.imageView);
         final ImageView sampleImage = findViewById(R.id.sampleImage);
 
         final ImageButton showBtn = (ImageButton) findViewById(R.id.showBtn);
         final ImageButton loadBtn = (ImageButton) findViewById(R.id.loadBtn);
         final ImageButton exitBtn = (ImageButton) findViewById(R.id.exitBtn);
+        final Button load = (Button) findViewById(R.id.load);
 
-        //zdarzenia dla przycisków
+        img = findViewById(R.id.imageView);
+//zdarzenia dla przycisków
         showBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (click == 0) {
@@ -56,10 +79,14 @@ public class PuzzleActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intentBack = new Intent(PuzzleActivity.this, MainActivity.class);
                 startActivity(intentBack);
+
+
             }
         });
 
         loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+
             public void onClick(View v) {
                 for (PuzzlePiece piece : parts) {
 
@@ -68,37 +95,69 @@ public class PuzzleActivity extends AppCompatActivity {
                     lParams.topMargin = layout.getHeight() - piece.pieceHeight;
                     piece.setLayoutParams(lParams);
                 }
-            }
-            ;
+            }                                       ;
         });
 
-        //start gry, wywołanie metod
-        imageView.post(new Runnable() {
-            @Override
-            public void run() {
-                parts = split();
-                TouchListener touchListener = new TouchListener(PuzzleActivity.this);
-                //wymieszanie puzzlo
-                Collections.shuffle(parts);
-                for (PuzzlePiece piece : parts) {
-                    piece.setOnTouchListener(touchListener);
-                    layout.addView(piece);
-                    //umieszczenie puzzli na dole ekranu
-                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) piece.getLayoutParams();
-                    lParams.leftMargin = new Random().nextInt(layout.getWidth() - piece.pieceWidth);
-                    lParams.topMargin = layout.getHeight() - piece.pieceHeight;
-                    piece.setLayoutParams(lParams);
+
+        load.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (click == 0) {
+
+                    Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    startActivityForResult(gallery, PICK_IMAGE);
+                    load.setText("Play");
+                    click = 2;
+                } else if(click ==2){
+                    img.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            sampleImage.setImageURI(imageUri);
+
+                            parts = split();
+
+                            TouchListener touchListener = new TouchListener(PuzzleActivity.this);
+//wymieszanie puzzlo
+                            Collections.shuffle(parts);
+                            for (PuzzlePiece piece : parts) {
+                                piece.setOnTouchListener(touchListener);
+                                layout.addView(piece);
+//umieszczenie puzzli na dole ekranu
+                                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) piece.getLayoutParams();
+                                lParams.leftMargin = new Random().nextInt(layout.getWidth() - piece.pieceWidth);
+                                lParams.topMargin = layout.getHeight() - piece.pieceHeight;
+                                piece.setLayoutParams(lParams);
+                            }
+                            ;
+                        }
+
+                        ;
+                    });
+                    load.setText("Again");
+
+                    click = 3;
+
                 }
-            }
-        });
-    }
+                else{
+                    Intent intentBack = new Intent(PuzzleActivity.this, PuzzleActivity.class);
+                    startActivity(intentBack);
 
+                    click = 3;
+                }
+                ;
+
+            }
+
+            ;
+
+//start gry, wywołanie metod
+
+        });
+    };
     public ArrayList<PuzzlePiece> split() {
         int piecesNumber = 12;
         int rows = 4;
         int cols = 3;
 
-        ImageView img = findViewById(R.id.imageView);
         ArrayList<PuzzlePiece> pieces = new ArrayList<>(piecesNumber);
 
         BitmapDrawable drawable = (BitmapDrawable) img.getDrawable();
@@ -115,10 +174,10 @@ public class PuzzleActivity extends AppCompatActivity {
 
         Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, puzzleWidth, puzzleHeight, true);
         Bitmap croppedBitmap = Bitmap.createBitmap(scaledBitmap, abs(puzzleLeft), abs(puzzleTop), croppedImageWidth, croppedImageHeight);
-        //obl wys i szer
+//obl wys i szer
         int pieceWidth = croppedImageWidth/cols;
         int pieceHeight = croppedImageHeight/rows;
-        //dodanie częśći puzzli do tablicy
+//dodanie częśći puzzli do tablicy
         int wspY = 0;
         for (int row = 0; row < rows; row++) {
             int wspX = 0;
@@ -169,7 +228,7 @@ public class PuzzleActivity extends AppCompatActivity {
 
         ret[2] = actualWidth;
         ret[3] = actualHeight;
-        //pozycja obrazka
+//pozycja obrazka
         int horizontal = imageView.getWidth();
         int vertical = imageView.getHeight();
 
@@ -184,7 +243,7 @@ public class PuzzleActivity extends AppCompatActivity {
 
 
     public void finish() {
-        //spr czy gameover
+//spr czy gameover
         if (GameOver()) {
             Toast.makeText(PuzzleActivity.this,"Wygrałeś!",Toast.LENGTH_SHORT).show();
             new android.os.Handler().postDelayed(
@@ -207,5 +266,17 @@ public class PuzzleActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requeastCode, int resultCode, Intent data) {
+        super.onActivityResult(requeastCode, resultCode,data);
+        if(resultCode==RESULT_OK && requeastCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            img.setImageURI(imageUri);
+        }
     }
 }
